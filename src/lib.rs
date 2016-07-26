3ac9808f59d7c16e112e1c028315fa74ba4184ad
David Ko <david@barobo.com>
2016-07-26 10:45:03 -0700

    Compiles again

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+extern crate protobuf;
+
 mod server_impl;
+mod rpc;
+
+use protobuf::Message;
+use std::io::Write;
 
 #[cfg(test)]
 mod tests {
@@ -7,20 +13,39 @@ mod tests {
     }
 }
 
-pub struct Server{
-    _server: server_impl::_Server
+pub struct Server<W:Write>{
+    _server: server_impl::_Server<W>
 }
 
-impl Server{
-    fn new() -> Server {
+impl<W:Write> Server<W>{
+    fn new() -> Server<W> {
         return Server { _server: server_impl::_Server::new() };
     }
 
     // Register a function to be called to handle FIRE requests
-    fn on<F>(&self, name: String, func: F)
+    fn on<F>(&mut self, name: String, func: F)
         where F: FnMut(Vec<u8>) -> Vec<u8>,
               F: 'static
     {
-        // TODO
+        self._server.on(name, func);
+    }
+
+    fn deliver(&mut self, data: Vec<u8>)
+    {
+        // 'data' should be a 'ClientMessage'
+        let cm_result = protobuf::parse_from_bytes::<rpc::ClientMessage>(data.as_slice());
+        match cm_result {
+            Ok(cm) => {
+            }
+            _ => {
+                // Return error reply
+                let mut reply = rpc::Reply::new();
+                let mut status = rpc::Reply_Status::new();
+                status.set_value(rpc::Status::DECODING_FAILURE);
+                reply.set_field_type(rpc::Reply_Type::STATUS);
+                reply.set_status(status);
+                self._server.write( reply.write_to_bytes().unwrap().as_slice() );
+            }
+        }
     }
 }
--- src/server_impl.rs
+++ src/server_impl.rs
@@ -1,20 +1,31 @@
 
 use std::collections::HashMap;
+use std::io::Write;
+use std::io;
 
-pub struct _Server {
-    fire_handlers: HashMap<String, Box<FnMut(Vec<u8>) -> Vec<u8>>>
+pub struct _Server<S:Write> {
+    fire_handlers: HashMap<String, Box<FnMut(Vec<u8>) -> Vec<u8>>>,
+    stream: Option<S>
 }
 
-impl _Server {
-    pub fn new() -> _Server {
+impl<S:Write> _Server<S> {
+    pub fn new() -> _Server<S> {
         return _Server {
-            fire_handlers : HashMap::new()
+            fire_handlers : HashMap::new(),
+            stream: None
         }
     }
 
-    fn on<F>(&mut self, name: String, func: F) 
+    pub fn on<F>(&mut self, name: String, func: F) 
         where F: FnMut(Vec<u8>) -> Vec<u8>,
               F: 'static {
         self.fire_handlers.insert(name, Box::new(func));
+    }
+
+    pub fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self.stream {
+            Some(ref mut stream) => { return stream.write(buf); }
+            None => { return Err(io::Error::new(io::ErrorKind::Other, "No available write stream.")); }
+        }
     }
 }
